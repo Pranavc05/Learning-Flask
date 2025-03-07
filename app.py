b28fbbda5e0124dc1e4f4f802b5f4091153894be
pranchan <pranchan@iu.edu>
2025-03-07 14:10:29 -0500

    SQLAlchemy database model and basic CSS styling SQLAlchemy for database management

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAchemy
 
 app=Flask(__name__)
+app.config['SQLAlCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAchemy(app)
+
+class Todo (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created= db.Column(db.)
 
 @app.route('/')
 def index():
