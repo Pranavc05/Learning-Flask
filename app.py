8ec6ae049da397b165f8d29d54064308f9a6224b
pranchan <pranchan@iu.edu>
2025-03-07 20:57:26 -0500

    Update app configuration and add database initialization
    - Fixed SQLAlchemy configuration and database model
    - Added datetime import and default date_created field
    - Implemented database creation in app context
    - Updated index route to support POST and GET methods
    - Minor template improvements

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI']='sqlite:///test.db'
-db=SQLAchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
 
 class Todo (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable = False)
-    date_created= db.Column(db.)
+    date_created= db.Column(db.DateTime,default=datetime.utcnow)
 
-@app.route('/')
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods={'POST','GET'})
 def index():
     return render_template('index.html')
 
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-
